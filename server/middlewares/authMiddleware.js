8921f20edbdfb46b5545113b90e36cd0d2882109
akimajid <akimajid07@gmail.com>
2024-09-12 23:20:32 +0700

    Cut some feature

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,18 +1,20 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const authenticateUser = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1]; // Mendapatkan token dari header
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: 'Authentication token is required' });
+    return res
+      .status(401)
+      .json({ message: "Authentication token is required" });
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verifikasi token JWT
-    req.user = decoded; // Simpan data user dari token ke req.user
-    next(); // Lanjutkan ke handler berikutnya
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
   } catch (error) {
-    return res.status(401).json({ message: 'Invalid token' });
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
 
